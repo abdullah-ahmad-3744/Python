37dbbc31e8e650db79488b8f14f01d8ff12b4e19
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-29 13:34:43 +0500

    Varaibles declaration in python .

--- 01-Day_1/Day_1.py
+++ 01-Day_1/Day_1.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print("First Day of Python Programming")
+# =======================================================
+
+# Variables in Python
+a = 10 
+b = 20 
+# Printing the values of a and b 
+print ("Value of a : ",a)
+print ("Value of b : ",b)
+
+# Sum of a and b a+b
+print ("Sum of", a , "and " , b, "= ",  a+b)
