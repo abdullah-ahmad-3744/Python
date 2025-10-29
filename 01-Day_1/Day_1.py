57186bb0f691d9bc39efb62fcce38a98a8bcbd89
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-29 15:36:24 +0500

    Greater than and less than in Comparision operators

--- 01-Day_1/Day_1.py
+++ 01-Day_1/Day_1.py
@@ -20,21 +20,27 @@ second_number = 10
 # Addition of two numbers 
 print ("Addition in Arithmatic Operators")
 print( "Sum of",first_number, "and", second_number, "=",   first_number + second_number)
+print()
 # Subtraction of two number
 print ("Subtraction in Arithmatic Operators")
 print("Subtraction from", first_number, "of", second_number, "=", first_number - second_number)
+print()
 # Multiplication of two numbers
 print ("Multiplication in Arithmatic Operators") 
 print (first_number,"* by",second_number , "=" ,first_number * second_number)
+print()
 # Division of two numbers 
 print ("Division in Arithmatic Operators")
 print(first_number , "Divided by", second_number , "=", first_number / second_number)
+print()
 # Remainder of two numbers 
 print ("Remainder in Arithmatic Operators")
 print ("Remainder of",first_number, "% ", second_number, "=",first_number % second_number)
+print()
 # Power of numbers
 print("Power Operator in Arithmatic Operator")
 print(first_number,"Power ",second_number, "=",first_number ** second_number)
+print()
 
 
 
@@ -42,13 +48,25 @@ print(first_number,"Power ",second_number, "=",first_number ** second_number)
 # Relational or Comparision operators in Operators
 # Equal to operator in Comparision operator
 print ("Checking if",first_number, "is equal to", second_number,"=",first_number == second_number)
+print()
 
 # Not Equal to Operator in Comparision Operators
 print("Checking if",first_number, "not equal to", second_number, "=",first_number != second_number)
+print()
+
+# Greater then Operator > in Comparision Operators
+print(first_number, "Is Greater than", second_number, "=" , first_number > second_number )
+print()
+
+# Less then Operator > in Comparision Operators
+print(first_number, "Is Less than", second_number, "=" , first_number < second_number )
+print()
 
 
 # Greater than Equal to (>=) Operator in Comparision Operator
 print("Checking if",first_number, ">=",second_number, "=",first_number >= second_number)
+print()
 
 # less than Equal to (<=) Operator in Comparision Operator
 print("Checking if",first_number, "<=",second_number, "=",first_number >= second_number)
+print()
