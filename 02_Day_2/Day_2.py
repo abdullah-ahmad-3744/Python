16609798d8412547da26aacf0b212daa46ff768a
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-30 22:09:49 +0500

    Searching and checking methods of the strings

--- 02_Day_2/Day_2.py
+++ 02_Day_2/Day_2.py
@@ -20,4 +20,35 @@ print("Without the strip method = ",user_name)
 print("With the strip method = ",user_name.strip())
 
 
-string_1 = "Hello today is day 2 of learning Python"
+
+
+string_1 = "Hello today is the day 2 of the learning Python"
+
+# replace() method
+print(string_1.replace("2", "two"))
+print()
+
+# endswith() method for checking the string ends with that word or not
+print(string_1.endswith("school"))
+print(string_1.endswith("Python"))
+print()
+
+# finds() method for finding a word or character in a string
+print(string_1.find("the"))
+print()
+
+# count() counts how many times a substring is present
+print(string_1.count("a"))
+print(string_1.count("the"))
+print("If the given substring is not present it returns ",string_1.count("college"))
+print()
+
+# startswith() checks if a string is started with a character or not
+print("If it does not starts with the substring then it returns = ",string_1.startswith("hi"))
+print("If it  starts with the substring then it returns = ",string_1.startswith("Hello"))
+print()
+
+# endswith() checks if a string ends with the given substring or not
+print("If it does not ends with the substring then it returns = ",string_1.endswith("python"))
+print("If it ends with the substring then it returns = ",string_1.endswith("Python"))
+print()
