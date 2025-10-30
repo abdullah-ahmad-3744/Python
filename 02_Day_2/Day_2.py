409acf692746e7f4b1d656b8785892378f062e4c
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-30 21:53:09 +0500

    Basic string methods

---
+++ 02_Day_2/Day_2.py
@@ -0,0 +1,23 @@
+# String Methods or functions
+user_name = "Ali"
+print("Length of " , user_name, "=",len(user_name))
+print()
+# lower() method
+print(user_name.lower())
+print()
+
+# upper() methos
+print(user_name.upper())
+print()
+
+# Capitalize method
+print(user_name.capitalize())
+print()
+
+# strip method
+user_name = "    Ali "
+print("Without the strip method = ",user_name)
+print("With the strip method = ",user_name.strip())
+
+
+string_1 = "Hello today is day 2 of learning Python"
