b99e6021a1b7e9a1efc9c53e2afc049ba4507cfe
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-31 22:42:50 +0500

    Most commonly used tuples methods

--- 03_Day_3/Day_3.py
+++ 03_Day_3/Day_3.py
@@ -1,3 +1,28 @@
 # Tuples in Python
-tuple1 = (1,2,3,4,5,6,7,8,9)
+tuple1 = (1,2,3,4,5,6,7,8,9, 6 , 3)
 print("The Type is ",type(tuple1))
+
+
+# Tuples methods
+# Most commonly used methods
+# len() methods to check the length of the tupel
+print()
+print("Length of tuple = ",len(tuple1))
+print()
+
+
+# count() method to count how many times an element is present 
+print(tuple1.count(6))
+print(tuple1.count(2))
+print()
+
+
+# max()  gives the highest or maximum value element in a tuple
+print("Maximum or highes value in the tuple = ",max(tuple1))
+print()
+
+
+# min() gives the lowest or minimum values
+print("Minimum or lowest value in tuple = ",min(tuple1))
+print()
+
