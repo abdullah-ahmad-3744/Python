9fbf751d49e02802a74330702c6376cec09a806c
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-10-31 23:31:07 +0500

    list practice questions

--- 03_Day_3/Practice.py
+++ 03_Day_3/Practice.py
@@ -1,2 +1,25 @@
-# Practice questions for tuples
+# Practice questions 
 
+# Write a program to ask user to enter their three favorite movies and store them in a list
+# favorite_movies = []
+# user_input = input("Enter your favorite movie :- ")
+# favorite_movies.append(user_input)
+# user_input = input("Enter your favorite movie :- ")
+# favorite_movies.append(user_input)
+# user_input = input("Enter your favorite movie :- ")
+# favorite_movies.append(user_input)
+# print(favorite_movies)
+
+
+# Write a program to check if a list contains palindrome or not
+list3 = [1,2,3,4,5]
+list2 = [1,2,3,2,1]
+copied_list = list2.copy()
+copied_list.reverse()
+print(list2)
+print(copied_list)
+if copied_list == list2:
+    print("Tuple is Palindrome")
+else:
+    print("Tuple is not Palindrome")
+    
