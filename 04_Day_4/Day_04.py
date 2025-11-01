dffaed2316efc3f1d8f935379b9e692932c2b0d4
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-01 21:13:53 +0500

    Accessing and Searching methods of Dictionary in Python

--- 04_Day_4/Day_04.py
+++ 04_Day_4/Day_04.py
@@ -37,3 +37,29 @@ print()
 print("Number of key-value pairs = ",len(dictionary2))
 print()
 print("Number of key-value pairs = ",len(dictionary2["marks"]))
+print()
+
+# Accessing and Searching methods of Dictionary 
+# keys() =====> returns all the keys of the dictionary
+print(dictionary2.keys())
+print()
+
+# values() =====> retruns the values of the keys
+print(dictionary2.values())
+print()
+
+# item() ====> returns the key-valaue pair as a separate tuple
+print(dictionary1.items())
+print()
+
+
+print(dictionary2.items())
+print()
+
+
+# get() ====> returns the value of key and returns none if key is invalid
+print(dictionary1.get("name"));
+print(dictionary2.get("address"))
+print()
+
+
