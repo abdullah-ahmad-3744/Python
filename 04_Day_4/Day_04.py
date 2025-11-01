99b68086de449c4508257b556f1fe21b181475a7
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-01 20:56:57 +0500

    Nested dictionaries in Python

--- 04_Day_4/Day_04.py
+++ 04_Day_4/Day_04.py
@@ -12,3 +12,16 @@ print()
 # Printing the value of a key
 print("Name = ",dictionary1["name"])
 print("Age = ",dictionary1["age"])
+print()
+
+dictionary2 = {
+    "name" : "Ali Ahmad",
+    "age" : 21,
+    "marks" : {
+        "English" : 82,
+        "Physics" : 73,
+        "Mathematics" : 90
+        }
+}
+print("Makrs of ",dictionary2["name"], "=",dictionary2["marks"])
+print("Marks in Physics = ", dictionary2["marks"]["Physics"])
