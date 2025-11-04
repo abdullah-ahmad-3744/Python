a45a50d012cc63ae6dffb64956e030e76255ebe1
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-04 17:48:43 +0500

    Adding and updating methods of the sets

--- 05_Day_5/Day.py
+++ 05_Day_5/Day.py
@@ -14,3 +14,24 @@ print(type(set1))
 # in   =====> Checks if an element exists in the set and returns True and False.
 print("If element is present in the set = ", 4 in set1)
 print("If element is not present in the set = ",100 in set1)
+print()
+print()
+
+
+
+
+# Adding and Updating Elements
+# add() =====> Adds an element ad the end of the list
+print("Set1 before adding Element = ", set1)
+set1.add(100)
+print("Set1 after adding Element = ", set1)
+print()
+print()
+
+
+# update() =====> Adds multiple elements from the list , tuple etc
+print("Set1 before updation = ", set1)
+set1.update([10,20,30,40])
+print("Set1 after updation = ", set1)
+print()
+print()
