4987b303a3cc3dc3a80999006c97335b624b3c83
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-04 17:42:19 +0500

    Basic and Property methods of sets

--- 05_Day_5/Day.py
+++ 05_Day_5/Day.py
@@ -2,3 +2,15 @@
 set1 = {1,2,3,4,5,6,7,8,9}
 print("Set = ",set1)
 print("Type = ",type(set1))
+
+# Basic and Property methods of sets 
+
+#  len() ===> Returns the length of the set
+print("Length of Set1 = ",len(set1))
+
+# type() returns the type of the set()
+print(type(set1))
+
+# in   =====> Checks if an element exists in the set and returns True and False.
+print("If element is present in the set = ", 4 in set1)
+print("If element is not present in the set = ",100 in set1)
