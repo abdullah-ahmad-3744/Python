01811923333ba33518acf3a02b990ce47ea238af
IT-01 <abdullahahmad3744@gmail.com>
2026-05-18 22:27:55 +0500

    Using f strings in Python

--- 07-Day7/Day_7.py
+++ 07-Day7/Day_7.py
@@ -3,17 +3,21 @@ print("Functions In Python:- ")
 
 # Funtions in Python
 
-def sum (num1, num2):
-    sum = num1 + num2
-    print(sum)
-
-
-sum(10,20)
-
+# def sum (num1, num2):
+#     sum = num1 + num2
+#     print(sum)
+# sum(10,20)
+# print("==============================================================================")
+# print(" ")
 # Creating a function for adding three numbers 
 def sum (num1, num2,num3):
     sum = num1 + num2 + num3
-    print(sum)
+    print(f"Sum of {num1},{num2} and {num3} = {sum}") 
+    
 sum(10,20,30)
-
-
+print("==============================================================================")
+print(" ")
+def sub (num1,num2):
+    sum = num1 - num2
+    print(sum)
+sub(50,10)
