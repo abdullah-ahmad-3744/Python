e7de751892108c2e4e7a27b17576692f4445ef5b
IT-01 <abdullahahmad3744@gmail.com>
2026-05-18 22:32:36 +0500

    Multiplication function

--- 07-Day7/Day_7.py
+++ 07-Day7/Day_7.py
@@ -17,7 +17,19 @@ def sum (num1, num2,num3):
 sum(10,20,30)
 print("==============================================================================")
 print(" ")
+
+# A function for substrcting two numbers
 def sub (num1,num2):
     sum = num1 - num2
     print(sum)
 sub(50,10)
+print("==============================================================================")
+print(" ")
+
+# A function for multiplying two numbers
+def sub (num1,num2):
+    sum = num1 * num2
+    print("Multiplication of",num1,"and",num2, "=", sum)
+sub(5,5)
+print("==============================================================================")
+print(" ")
