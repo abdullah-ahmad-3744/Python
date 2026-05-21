f52b187d14096cdbeebbcacf6451242852476a98
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-05-21 22:46:59 +0500

    Day-09 of practicing Python

---
+++ 09-Day-9/Day-09.py
@@ -0,0 +1,7 @@
+# You are given a number  and you have to print your answer according to the following:
+# If the number is divisible by 3, you print "Fizz" (without quotes)
+# If the number is divisible by 5, you print "Buzz" (without quotes)
+# If the number is divisible by both 3 and 5, you print "FizzBuzz" (without quotes)
+# In any other case, you print the number itself
+
+# Note: You should add a newline character after print() statement.
