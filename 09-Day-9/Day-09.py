2ca6cc588681bb7e5c9a33bba549d84152fd4242
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-05-21 23:41:03 +0500

    Question 1 resolved on Day 9 of Python practice

--- 09-Day-9/Day-09.py
+++ 09-Day-9/Day-09.py
@@ -6,3 +6,13 @@
 
 # Note: You should add a newline character after print() statement.
 print("Hello World")
+
+user_input = int(input("Enter a number:- "))
+if user_input % 3 == 0 and user_input % 5 == 0 :
+    print("FizzBuzz")
+elif user_input % 3 == 0:
+    print("Fizz")
+elif user_input % 5 == 0 :
+    print("Buzz")
+else:
+    print(user_input)
