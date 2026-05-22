e2ac028c22ac338ca1b87ccb9ead69483dd72e06
IT-01 <abdullahahmad3744@gmail.com>
2026-05-22 12:19:40 +0500

    Small changes

--- 10-Day-10/Question_3.py
+++ 10-Day-10/Question_3.py
@@ -1,7 +1,7 @@
 # Given two numbers a and b; you need to perform basic mathematical operation on them. You will be provided an integer named as operator. 
-# If operator equals to 1 add a and b, then print the result as a string.
-# If operator equals to 2 subtract b from a, then print the result as a string.
-# If operator equals to 3 multiply a and b, then print the result as a string.
+# If operator equals to 1 add a and b, then print the result 
+# If operator equals to 2 subtract b from a, then print the result 
+# If operator equals to 3 multiply a and b, then print the result 
 # If operator equals to any another number, print "Invalid Input"(without quotes).
 
 
