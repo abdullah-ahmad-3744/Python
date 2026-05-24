6393b97e6f33e9e4a13e2e5ded2243fb4bc9c030
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-05-24 23:36:29 +0500

    Reading a file line by line using readline() method

--- 11-Day_11/Day_11.py
+++ 11-Day_11/Day_11.py
@@ -5,9 +5,16 @@
 file_1 = open("demo.txt", "r")
 print(file_1.read())
 
+
 # Reading the specific or limited number of characters 
-file_1 = open("demo.txt", "r")
-print(file_1.read(5))
+file_2 = open("demo.txt", "r")
+print(file_2.read(7))
 
+# Reading the file line by line
+# First line 
+line_1 = file_2.readline()
+print("First Line in Demo File = ",line_1)
 
-    
+# Reading 2nd line in a file
+second_line = file_2.readline()
+print("Second Line ====== ", second_line)
