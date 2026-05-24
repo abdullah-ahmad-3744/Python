f6df696be85c0fa64ca732ad1f45f2aede6949cb
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-05-24 23:05:27 +0500

    File I/O in Python on Day 11 of Python practice

--- 11-Day_11/Question_1.py
+++ 11-Day_11/Question_1.py
@@ -1,2 +1,5 @@
-# Given a tuple arr , print "True" if all elements of tuple are different otherwise print "False".
-# A tuple is a collection of items that are ordered and unchangeable.
+# File I/O in Python
+
+
+
+    
