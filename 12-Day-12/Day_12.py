cc5d81a429ae5d3ecb170b9e7289d64360dc8089
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-02 22:17:55 +0500

    Question number 1 while practicing file i/o

--- 12-Day-12/Day_12.py
+++ 12-Day-12/Day_12.py
@@ -1,1 +1,10 @@
 # Day 12 of Python, Practice questions related to file i/o 
+# Question 1 
+# Create a new file "practice.txt" using python. Add the following in it. 
+# "Hi everyone 
+# we are learning File i/o
+# using Java
+# I like programming in Java"
+
+file_1 = open("sample.txt", "w")
+file_1.write("Hi everyone \n we are learning File i/o \n using Python \n I like programming in Python")
