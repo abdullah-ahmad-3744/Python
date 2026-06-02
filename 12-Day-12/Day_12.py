871d5e8ad2e0545a5fe1229ad9a9890e72d56f10
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-02 22:34:09 +0500

    Solving question by 'with syntax' approach

--- 12-Day-12/Day_12.py
+++ 12-Day-12/Day_12.py
@@ -1,5 +1,7 @@
 # Day 12 of Python, Practice questions related to file i/o 
+
 # Question 1 
+
 # Create a new file "practice.txt" using python. Add the following in it. 
 # "Hi everyone 
 # we are learning File i/o
@@ -8,3 +10,9 @@
 
 file_1 = open("sample.txt", "w")
 file_1.write("Hi everyone \n we are learning File i/o \n using Python \n I like programming in Python")
+
+# Solving question using with syntax approach
+with open("practice.txt", "w") as f :
+    f.write("Hi everyone \n we are learning File i/o \n using Python \n I like programming in Python")
+
+# Write a function that replaces all the occurance of the Java with Python
