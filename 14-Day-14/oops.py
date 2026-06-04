05c470d73065803728060318e0e01db571801756
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-04 22:58:00 +0500

    Passing dynamic values of properties using self parameter of constructor

--- 14-Day-14/oops.py
+++ 14-Day-14/oops.py
@@ -1,8 +1,11 @@
 print("OOP'S in Python ")
 
 class Students:
-    def __init__(self):
-        print('Constructor of Class Student')
-
-
-student_1 = Students()
+    def __init__(self,name,age):
+        print('Constructor of Class Student...')
+        self.name = name
+        self.age = age
+        
+student_1 = Students("Ali Ahmad", 24)
+print("Studnet_1 name : ",student_1.name)
+print("Student_1 age : ",student_1.age)
