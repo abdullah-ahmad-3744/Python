3a3ef32c5c7e046ae25fb698834bb75353cbf1bc
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-05 23:20:26 +0500

    Question answer corrected

--- 14-Day-14/Question_1.py
+++ 14-Day-14/Question_1.py
@@ -1,15 +1,11 @@
 # Create a class Student that takes name and marks of 3 subjects as arguments in constructor. Then create a method to print the averge
-print("Hello")
 class Students :
-    def __init__ (self,sub_1,sub_2,sub_3,marks_1,marks_2,marks_3):
-        self.sub_1 = sub_1
-        self.sub_2 = sub_2
-        self.sub_3 = sub_3
-        self.marks_1 = marks_1
-        self.marks_2 = marks_2
-        self.marks_3 = marks_3
-    def average (self) :
-        print(f"Average of {self.marks_1} , {self.marks_2} and {self.marks_3} = {(self.marks_1 + self.marks_2 + self.marks_3)/3}")
+    def __init__ (self,name,marks):
+        self.name = name
+        self.marks = marks
+         
+    def get_average (self) :
+        print((self.marks[0] +  self.marks[1] +  self.marks[2])/ len(self.marks))
 
-student_1 = Students("a", "b", "c", 10,20,30)
-student_1.average()
+student_1 = Students("Abdullah Ahamad", [10,20,30])
+student_1.get_average()
