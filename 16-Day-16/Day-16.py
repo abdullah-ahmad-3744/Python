b0d861efda3149bd38f2cfd2eb8027e19c3a7978
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-06 23:34:45 +0500

    Practicing Abstraction in OOP with Python

--- 16-Day-16/Day-16.py
+++ 16-Day-16/Day-16.py
@@ -1,1 +1,25 @@
 # Abstraction in Python
+
+class Cars :
+    def __init__ (self) :
+        self.accelrator = False
+        self.brk = False
+        self.clutch = False
+        self.grear  = 0
+    
+    def start_car (self) :
+        self.accelrator = True
+        self.clutch = True
+        self.grear = 1
+        print("Car Started...")
+    def stop_car (self) :
+        self.accelrator = False
+        self.brk = True
+        self.clutch = True
+        print("Car stopped...")
+
+
+car_1 = Cars()
+car_1.start_car()
+print("=========================================================")
+car_1.stop_car()
