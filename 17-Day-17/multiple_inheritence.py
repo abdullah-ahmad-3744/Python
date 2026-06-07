61687507bbc06257d048b393417a6083d7652d64
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-08 00:04:22 +0500

    Syntax error fixed

--- 17-Day-17/multiple_inheritence.py
+++ 17-Day-17/multiple_inheritence.py
@@ -1,12 +1,12 @@
 # Multiple inheritence in Python
 class A :
-    variable_a : "Welcome to variable A"
+    variable_a = "Welcome to variable A"
 
 class B :
-    variable_b : "Welcome to variable B"
+    variable_b = "Welcome to variable B"
 
 class C (A,B):
-    variable_c : "Welcome to variable C"
+    variable_c =  "Welcome to variable C"
 
 c1 = C()
 print(c1.variable_c)
