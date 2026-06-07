1e0140a9ee2d930708e849f1cecb3f01eb7e0b8f
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-07 23:11:09 +0500

    Accessing the private attributes

--- 17-Day-17/Day_17.py
+++ 17-Day-17/Day_17.py
@@ -22,7 +22,11 @@ class Account :
     def __init__ (self, account_number, account_password) :
         self.account_number= account_number
         self.__account_password = account_password
+        
+    def print_password (self) :
+            print(self.__account_password)
 
 account_1 = Account(1234567890, 1122)
 print("Account number : ",account_1.account_number)
-print("Account password : ",account_1.__account_password)
+# print("Account password : ",account_1.__account_password) // Error trying to access a private attribute out of the scope
+print(account_1.print_password())
