152952eb57993bace99a97a7154e1749d84531ca
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-07 22:26:24 +0500

    Account password is accessible outside the class scope , because account password is a public attribute

--- 17-Day-17/Day_17.py
+++ 17-Day-17/Day_17.py
@@ -9,8 +9,20 @@ std_1 = Students("Muhammad Abdullah")
 std_2 = Students('Ali Ahmad')
 print(std_1.name)
 # deleting a property of an object
-del std_1.name
-print(std_1.name)
+# del std_1.name
+# print(std_1.name)
 # deleting an object
 del std_2
-print(std_2)
+# print(std_2)
+
+
+# Private and Public attributes
+
+class Account :
+    def __init__ (self, account_number, account_password) :
+        self.account_number= account_number
+        self.account_password = account_password
+
+account_1 = Account(1234567890, 1122)
+print("Account number : ",account_1.account_number)
+print("Account password : ",account_1.account_password)
