d20321e9ee14418c1cf8cd6c02a6705696e77a48
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-07 21:53:35 +0500

    deleting the property of an object and a complete object

--- 17-Day-17/Day_17.py
+++ 17-Day-17/Day_17.py
@@ -1,1 +1,16 @@
 print("Day 17 of learning Python")
+
+class Students :
+    def __init__(self, name) :
+        self.name = name
+
+
+std_1 = Students("Muhammad Abdullah")
+std_2 = Students('Ali Ahmad')
+print(std_1.name)
+# deleting a property of an object
+del std_1.name
+print(std_1.name)
+# deleting an object
+del std_2
+print(std_2)
